0a60123ac9098d3c36cb0c106eb7a883278c39fc
cpwoo <world9969@khu.ac.kr>
2024-05-02 16:25:15 +0900

    #1 - Flow Service 모듈 생성
    대기열 시스템을 관리하는 Flow Service 모듈을 생성했다.
    비동기 방식으로 관리되며 성능 향상을 목표로 한다.

---
+++ flow-service/src/test/java/com/ticketcheater/flowservice/FlowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketcheater.flowservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
