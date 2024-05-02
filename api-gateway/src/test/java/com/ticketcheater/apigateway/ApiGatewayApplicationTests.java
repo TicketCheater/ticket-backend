e5957479f8282e05440c70253fd52e5023dc79d1
cpwoo <world9969@khu.ac.kr>
2024-05-02 16:21:10 +0900

    #1 - API Gateway 모듈 생성
    Back-End 서비스에 대해 호출, 관리, 모니터링, 보안을 담당하는 API Gateway 모듈을 생성했다.

---
+++ api-gateway/src/test/java/com/ticketcheater/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketcheater.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
