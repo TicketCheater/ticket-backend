fdc06b43bbe164e373107d674eb453d86359142b
cpwoo <world9969@khu.ac.kr>
2024-05-14 23:44:05 +0900

    #15 - Response 기본 생성자 어노테이션 제거
    DTO 에서 JSON 으로 직렬화 할 때는 기본 생성자가 필요하지는 않다.
    import 문을 최적화했다.

--- web-service/src/main/java/com/ticketcheater/webservice/controller/response/member/MemberReissueResponse.java
+++ web-service/src/main/java/com/ticketcheater/webservice/controller/response/member/MemberReissueResponse.java
@@ -2,10 +2,8 @@ package com.ticketcheater.webservice.controller.response.member;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class MemberReissueResponse {
 
