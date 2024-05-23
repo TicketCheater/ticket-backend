663384e1fb6ccc6066d5404c47e7adef52d967e9
cpwoo <world9969@khu.ac.kr>
2024-05-23 18:51:02 +0900

    #35 - JPQL 추가
    JPA 가 의도한 것과 다르게 동작하는 것을 포착했다.
    쿼리가 조금만 복잡해져도 이런 문제가 생기니 고민의 여지가 생겼다.

--- web-service/src/main/java/com/ticketcheater/webservice/repository/GameRepository.java
+++ web-service/src/main/java/com/ticketcheater/webservice/repository/GameRepository.java
@@ -3,12 +3,19 @@ package com.ticketcheater.webservice.repository;
 import com.ticketcheater.webservice.entity.Game;
 import com.ticketcheater.webservice.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    Optional<Game> findByIdAndDeletedAtIsNull(Long id);
-    Optional<Game> findByIdAndDeletedAtIsNotNull(Long id);
-    List<Game> findByHomeAndDeletedAtIsNull(Team home);
+    @Query("SELECT g FROM game g WHERE g.id = :id AND g.deletedAt IS NULL")
+    Optional<Game> findByIdAndDeletedAtIsNull(@Param("id") Long id);
+
+    @Query("SELECT g FROM game g WHERE g.id = :id AND g.deletedAt IS NOT NULL")
+    Optional<Game> findByIdAndDeletedAtIsNotNull(@Param("id") Long id);
+
+    @Query("SELECT g FROM game g WHERE g.home = :home AND g.deletedAt IS NULL")
+    List<Game> findByHomeAndDeletedAtIsNull(@Param("home") Team home);
 }
--- web-service/src/main/java/com/ticketcheater/webservice/repository/GradeRepository.java
+++ web-service/src/main/java/com/ticketcheater/webservice/repository/GradeRepository.java
@@ -1,13 +1,20 @@
 package com.ticketcheater.webservice.repository;
 
 import com.ticketcheater.webservice.entity.Grade;
+import com.ticketcheater.webservice.entity.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface GradeRepository extends JpaRepository<Grade, Long> {
-    Optional<Grade> findByPlaceIdAndName(Long placeId, String name);
-    Optional<Grade> findByIdAndDeletedAtIsNull(Long gradeId);
-    Optional<Grade> findByIdAndDeletedAtIsNotNull(Long gradeId);
+    @Query("SELECT g FROM grade g WHERE g.place = :place AND g.name = :name")
+    Optional<Grade> findByPlaceAndName(@Param("place") Place place, @Param("name") String name);
 
+    @Query("SELECT g FROM grade g WHERE g.id = :id AND g.deletedAt IS NULL")
+    Optional<Grade> findByIdAndDeletedAtIsNull(@Param("id") Long id);
+
+    @Query("SELECT g FROM grade g WHERE g.id = :id AND g.deletedAt IS NOT NULL")
+    Optional<Grade> findByIdAndDeletedAtIsNotNull(@Param("id") Long id);
 }
