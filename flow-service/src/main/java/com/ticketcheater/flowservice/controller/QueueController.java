f62a6ddd31e084fe5fb4aa2775345311a6d84fb1
cpwoo <world9969@khu.ac.kr>
2024-05-28 23:08:05 +0900

    #47 - 대기열 서비스 구현
    대기 큐에 등록 -> 스케줄러를 이용해 진행 큐로 이동 -> 진행 큐에 있으면 토큰 주고 없으면 대기 큐 순위 주는 로직을 구현했다.
    일단 스케줄러를 쓸려면 main 메서드가 있는 클래스에
    `@EnableScheduling` 어노테이션을 달아줘야 한다.

---
+++ flow-service/src/main/java/com/ticketcheater/flowservice/controller/QueueController.java
@@ -0,0 +1,59 @@
+package com.ticketcheater.flowservice.controller;
+
+import com.ticketcheater.flowservice.controller.response.RankResponse;
+import com.ticketcheater.flowservice.controller.response.Response;
+import com.ticketcheater.flowservice.controller.response.TokenResponse;
+import com.ticketcheater.flowservice.service.QueueService;
+import com.ticketcheater.flowservice.util.JwtTokenParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/v1/flow/")
+@RequiredArgsConstructor
+public class QueueController {
+
+    private final JwtTokenParser tokenParser;
+    private final QueueService queueService;
+
+    @PostMapping("register/{gameId}")
+    public Mono<Response<RankResponse>> registerQueue(
+            @PathVariable("gameId") String gameId,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String header
+    ) {
+        String name = tokenParser.getName(header);
+        return queueService.registerWaitQueue(gameId, name)
+                .flatMap(rank -> Response.success(new RankResponse(rank)));
+    }
+
+    @GetMapping("/{gameId}")
+    public Mono<Response<?>> getInfo(
+            @PathVariable("gameId") String gameId,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String header,
+            ServerWebExchange exchange
+    ) {
+        String name = tokenParser.getName(header);
+        return queueService.processMember(gameId, name)
+                .flatMap(token -> {
+                    if (token != null) {
+                        exchange.getResponse().addCookie(
+                                ResponseCookie.from("member-queue-%s-token".formatted(gameId), token)
+                                        .maxAge(Duration.ofSeconds(300))
+                                        .path("/")
+                                        .build()
+                        );
+                        return Response.success(new TokenResponse(token));
+                    } else {
+                        return queueService.getRank(gameId, name)
+                                .flatMap(rank -> Response.success(new RankResponse(rank)));
+                    }
+                });
+    }
+
+}
---
+++ flow-service/src/main/java/com/ticketcheater/flowservice/service/QueueService.java
@@ -0,0 +1,88 @@
+package com.ticketcheater.flowservice.service;
+
+import com.ticketcheater.flowservice.exception.ErrorCode;
+import com.ticketcheater.flowservice.exception.FlowApplicationException;
+import com.ticketcheater.flowservice.util.TokenGenerator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class QueueService {
+
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
+
+    private static final String MEMBERS_QUEUE_WAIT_KEY = "members:queue:%s:wait";
+    private static final String MEMBERS_QUEUE_SCAN = "members:queue:*:wait";
+    private static final String MEMBER_QUEUE_PROCEED_KEY = "members:queue:%s:proceed";
+    private static final Long ALLOW_COUNT = 100L;
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling;
+
+    public Mono<Long> registerWaitQueue(String gameId, String name) {
+        long timestamp = System.currentTimeMillis();
+        return reactiveRedisTemplate.opsForZSet().add(MEMBERS_QUEUE_WAIT_KEY.formatted(gameId), name, timestamp)
+                .filter(i -> i)
+                .switchIfEmpty(Mono.error(() -> new FlowApplicationException(ErrorCode.ALREADY_REGISTERED_MEMBER, String.format("%s is already registered", name))))
+                .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(MEMBERS_QUEUE_WAIT_KEY.formatted(gameId), name))
+                .map(i -> i>=0 ? i+1 : i);
+    }
+
+    public Mono<Long> getRank(String gameId, String name) {
+        return reactiveRedisTemplate.opsForZSet().rank(MEMBERS_QUEUE_WAIT_KEY.formatted(gameId), name)
+                .defaultIfEmpty(-1L)
+                .map(i -> i>=0 ? i+1 : i);
+    }
+
+    public Mono<Long> allowMember(String gameId) {
+        return reactiveRedisTemplate.opsForZSet().popMin(MEMBERS_QUEUE_WAIT_KEY.formatted(gameId), ALLOW_COUNT)
+                .flatMap(member -> {
+                    if (member == null || member.getValue() == null) {
+                        log.warn("Member or its value is null.");
+                        return Mono.just(0L);
+                    }
+                    return reactiveRedisTemplate.opsForSet().add(MEMBER_QUEUE_PROCEED_KEY.formatted(gameId), member.getValue());
+                })
+                .count();
+    }
+
+    public Mono<String> processMember(String gameId, String name) {
+        return reactiveRedisTemplate.opsForSet().remove(MEMBER_QUEUE_PROCEED_KEY.formatted(gameId), name)
+                .flatMap(count -> {
+                    if(count == 1) {
+                        return Mono.just(TokenGenerator.generateToken(gameId, name));
+                    } else {
+                        return Mono.empty();
+                    }
+                });
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowMember() {
+        if(!scheduling) {
+            log.info("passed scheduling...");
+            return;
+        }
+        log.info("called scheduling...");
+
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                .match(MEMBERS_QUEUE_SCAN).count(ALLOW_COUNT).build())
+                .flatMap(this::extractGameIdAndAllowMember)
+                .subscribe();
+    }
+
+    private Mono<Long> extractGameIdAndAllowMember(String key) {
+        String gameId = key.split(":")[2];
+        log.info("Allowed members of %s queue".formatted(gameId));
+        return allowMember(gameId);
+    }
+
+}
